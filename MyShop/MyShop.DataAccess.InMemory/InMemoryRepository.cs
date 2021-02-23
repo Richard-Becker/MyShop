664815c066e9937b3c35bedf766d8dc0d373041d
Richard-Becker <mr.erbecker@gmail.com>
2021-02-23 22:08:36 +0200

    Created InMemoryRepository Interface. Updated Product and Category ManagerControllers with Dependency Injection. Added DI Container.

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
+using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity
-
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         private ObjectCache cache = MemoryCache.Default;
         private List<T> items;
@@ -29,7 +29,7 @@ namespace MyShop.DataAccess.InMemory
         {
             cache[className] = items;
         }
-
+        
         public void Insert(T t)
         {
             items.Add(t);
